0f1f478d144eaaccedac1b90e77380ea5dea27fc
kevin20040426-beep <kevin20040426@gmail.com>
2025-12-11 17:42:21 +0800

    feat: Configure Firebase Analytics
    Integrates Firebase Analytics into the SmartWealth application to enable tracking of user engagement and application usage.

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: 請在 Firebase Console (https://console.firebase.google.com/) 建立專案
-// 並將生成的設定檔複製到下方。
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBnKAaJsfQdK75khAWw5s7eTe16K58MuRk",
+  authDomain: "project-488128208390334691.firebaseapp.com",
+  projectId: "project-488128208390334691",
+  storageBucket: "project-488128208390334691.firebasestorage.app",
+  messagingSenderId: "916786384920",
+  appId: "1:916786384920:web:4f4c161bdee3844be2b36d",
+  measurementId: "G-BRX1XML46T"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
